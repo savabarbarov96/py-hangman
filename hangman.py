5710d63221ad84dc757223e54576c5f5626ea6f4
Sava Barbarov <savabarbarov96@gmail.com>
2023-08-16 18:32:46 +0300

    Implemented word and character display, ability to quit and restart the game and quality of life chganes

--- hangman.py
+++ hangman.py
@@ -1,13 +1,14 @@
 import requests
-
+import os
 
 def hangman():
     response = requests.get("https://random-word-api.herokuapp.com/word")
-    word = response.json()
-    word_chars = set(word[0])  # Convert to set for faster membership testing
+    word = response.json()[0]
+    word_chars = set(word)
 
+    print(word)
     score = 5
-    guessed_chars = set()  # To keep track of guessed characters
+    guessed_chars = set()
 
     while score > 0:
 
@@ -15,27 +16,103 @@ def hangman():
 
         if guess in guessed_chars:
             print("You've already guessed that letter. Try again.")
-            continue  # Skip the rest of the loop and go to the next iteration
+            continue
 
-        guessed_chars.add(guess)  # Add the guessed character to the set
+        if len(guess) != 1 or not guess.isalpha():
+            print("Please enter a single letter.")
+            continue
 
-        if len(guess) > 1:
-            print("You can only guess one character at a time.")
-            continue    # Only accept 1 character
+        guessed_chars.add(guess)
 
         if guess in word_chars:
             print("Correct!")
-            # Remove guessed letter from set to avoid counting it again
             word_chars.remove(guess)
-
-            if len(word_chars) == 0:  # If all letters have been guessed
+            if len(word_chars) == 0:
                 print("You win!")
-                break
+                play_again = input("Do you want to play again? Yes/No\n")
+                if play_again == "yes":
+                    hangman()
+                if play_again == "no":
+                    print("Thank you for playing!")
+                    return
         else:
             print("Incorrect. You have", score - 1, "guesses left.")
             score -= 1
 
-    return "You lose. The word was " + word[0]
+        display_word = ""
+        for letter in word:
+            if letter in guessed_chars:
+                display_word += letter
+            else:
+                display_word += "_"
+        print(display_word)
 
+        if score == 0:
+            print("You lose. The word was", word)
+            play_again = input("Do you want to play again? Yes/No\n")
+            if play_again == "yes":
+                hangman()
+            if play_again == "no":
+                print("Thank you for playing!")
+                return
+
+hangman()
+import requests
+import os
+
+def hangman():
+    response = requests.get("https://random-word-api.herokuapp.com/word")
+    word = response.json()[0]
+    word_chars = set(word)
+
+    print(word)
+    score = 5
+    guessed_chars = set()
+
+    while score > 0:
+
+        guess = input("Try to guess my word, type a letter: ").lower()
+
+        if guess in guessed_chars:
+            print("You've already guessed that letter. Try again.")
+            continue
+
+        if len(guess) != 1 or not guess.isalpha():
+            print("Please enter a single letter.")
+            continue
+
+        guessed_chars.add(guess)
+
+        if guess in word_chars:
+            print("Correct!")
+            word_chars.remove(guess)
+            if len(word_chars) == 0:
+                print("You win!")
+                play_again = input("Do you want to play again? Yes/No\n")
+                if play_again == "yes":
+                    hangman()
+                if play_again == "no":
+                    print("Thank you for playing!")
+                    return
+        else:
+            print("Incorrect. You have", score - 1, "guesses left.")
+            score -= 1
+
+        display_word = ""
+        for letter in word:
+            if letter in guessed_chars:
+                display_word += letter
+            else:
+                display_word += "_"
+        print(display_word)
+
+        if score == 0:
+            print("You lose. The word was", word)
+            play_again = input("Do you want to play again? Yes/No\n")
+            if play_again == "yes":
+                hangman()
+            if play_again == "no":
+                print("Thank you for playing!")
+                return
 
 hangman()
